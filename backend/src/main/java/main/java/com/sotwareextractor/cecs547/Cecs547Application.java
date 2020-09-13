bd651fda9fde1ae74e00baa46736b4ac8ce2de1e
mcurci95 <mcurci95@gmail.com>
2020-09-12 18:25:18 -0700

    Adding Spring initialization files

---
+++ backend/src/main/java/main/java/com/sotwareextractor/cecs547/Cecs547Application.java
@@ -0,0 +1,13 @@
+package main.java.com.sotwareextractor.cecs547;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cecs547Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cecs547Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sotwareextractor/cecs547/Cecs547ApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.sotwareextractor.cecs547;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cecs547ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
