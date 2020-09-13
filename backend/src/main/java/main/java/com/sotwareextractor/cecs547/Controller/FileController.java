6187fd9fa9eeb20d499b65bd02603a944715c51d
mcurci95 <mcurci95@gmail.com>
2020-09-12 19:14:30 -0700

    FileController class to upload file

---
+++ backend/src/main/java/main/java/com/sotwareextractor/cecs547/Controller/FileController.java
@@ -0,0 +1,27 @@
+package main.java.com.sotwareextractor.cecs547.Controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+
+@CrossOrigin("*")
+@RestController
+public class FileController {
+
+    Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity uploadFile(@RequestParam MultipartFile file) {
+        logger.info("Logging file from FileController. File name %s", file.getOriginalFilename());
+
+        return ResponseEntity.ok().build();
+    }
+}
