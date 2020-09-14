70f309b5119db0772f7dbc8f4370a6ae6ba86906
mcurci95 <mcurci95@gmail.com>
2020-09-13 20:24:42 -0700

    renaming controller class to be consistent

--- backend/src/main/java/main/java/com/sotwareextractor/cecs547/Controller/SourceFileController.java
+++ backend/src/main/java/main/java/com/sotwareextractor/cecs547/Controller/SourceFileController.java
@@ -1,7 +1,9 @@
 package main.java.com.sotwareextractor.cecs547.Controller;
 
+import main.java.com.sotwareextractor.cecs547.Service.SourceFileStorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,13 +12,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-
 @CrossOrigin("*")
 @RestController
-public class FileController {
+public class SourceFileController {
 
-    Logger logger = LoggerFactory.getLogger(FileController.class);
+    @Autowired
+    private SourceFileStorageService sourceFileStorageService;
+    
+    Logger logger = LoggerFactory.getLogger(SourceFileController.class);
 
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity uploadFile(@RequestParam MultipartFile file) {
