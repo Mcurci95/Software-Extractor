3683b073c735892572a894b2699f375bde8241c7
mcurci95 <mcurci95@gmail.com>
2020-09-14 23:15:15 -0700

    Adding default constructor so we ca use findAll()

--- backend/src/main/java/main/java/com/sotwareextractor/cecs547/Controller/SourceFileController.java
+++ backend/src/main/java/main/java/com/sotwareextractor/cecs547/Controller/SourceFileController.java
@@ -1,18 +1,17 @@
 package main.java.com.sotwareextractor.cecs547.Controller;
 
+import main.java.com.sotwareextractor.cecs547.Model.SourceFile;
 import main.java.com.sotwareextractor.cecs547.Service.SourceFileStorageService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @CrossOrigin("*")
 @RestController
@@ -30,5 +29,13 @@ public class SourceFileController {
             sourceFileStorageService.saveFile(file);
         }
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/fileNames")
+    @ResponseBody
+    public List<SourceFile> getFiles() {
+        logger.info("Grabbing all source files");
+        return sourceFileStorageService.getFiles();
+
     }
 }
