41775a471ca864055aebba0d8c03098394a32eee
mcurci95 <mcurci95@gmail.com>
2020-09-21 00:13:42 -0700

    Creating post request and adding onSubmit

--- frontend/software-extractor/src/components/FileUploader.js
+++ frontend/software-extractor/src/components/FileUploader.js
@@ -5,6 +5,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import FormGroup from 'react-bootstrap/esm/FormGroup';
+import axios from 'axios';
+
+const UPLOAD_ENDPOINT = 'http://localhost:8080/upload';
 
 const styles = {
     display: 'flex',
@@ -13,10 +16,35 @@ const styles = {
     justifyContent: 'center'
 };
 
+const onChange = e => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+}
+
+const onSubmit = async e => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('files', file);
+
+    try {
+        const res = await axios.post(UPLOAD_ENDPOINT, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
 const FileUploader = () => {
+    // Starting with one file for testing then using multiple
+    const [file, setFile] = useState('');
+    const [fileName, setFileName] = useState('');
+
     return (
         <div style={styles}>
-        <Form>
+        <Form onSubmit={ uploadFiles }>
         <Row style={styles}>
                 <Col>
                     <Form.Group>
