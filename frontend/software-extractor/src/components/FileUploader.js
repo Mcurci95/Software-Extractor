d6cf5c9485c49651aadada670d23231e54bac2e3
mcurci95 <mcurci95@gmail.com>
2020-09-26 12:40:29 -0700

    Changing function locaations

--- frontend/software-extractor/src/components/FileUploader.js
+++ frontend/software-extractor/src/components/FileUploader.js
@@ -16,35 +16,39 @@ const styles = {
     justifyContent: 'center'
 };
 
-const onChange = e => {
-    setFile(e.target.files[0]);
-    setFileName(e.target.files[0].name);
-}
 
-const onSubmit = async e => {
-    e.preventDefault();
 
-    const formData = new FormData();
-    formData.append('files', file);
-
-    try {
-        const res = await axios.post(UPLOAD_ENDPOINT, formData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        });
-    } catch (err) {
-        console.error(err);
-    }
-}
 const FileUploader = () => {
     // Starting with one file for testing then using multiple
     const [file, setFile] = useState('');
     const [fileName, setFileName] = useState('');
 
+    const onChange = e => {
+        setFile(e.target.files[0]);
+        setFileName(e.target.files[0].name);
+    }
+
+
+    const onSubmit = async e => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append('files', file);
+
+        try {
+            const res = await axios.post(UPLOAD_ENDPOINT, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <div style={styles}>
-        <Form onSubmit={ uploadFiles }>
+        <Form onSubmit={ onSubmit }>
         <Row style={styles}>
                 <Col>
                     <Form.Group>
