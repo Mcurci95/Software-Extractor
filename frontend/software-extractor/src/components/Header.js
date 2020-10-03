8f077ffaa0cc2083dd707c1581833ff535c422e2
Dennis Lo <kawai924@gmail.com>
2020-10-03 00:19:50 -0700

    Add files via upload

---
+++ frontend/software-extractor/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+const Header = () =>{
+
+    return (
+        <div id="nav">
+          <ul>
+           <li><a href="#">Team DML</a></li>
+          </ul>
+        </div>
+    );
+
+}
+
+export default Header;
