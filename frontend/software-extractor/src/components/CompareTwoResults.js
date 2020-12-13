e3fdc703d7b9200052c2b14b5e217e7a35abf567
mcurci95 <mcurci95@gmail.com>
2020-12-12 21:05:19 -0800

    Added version1 and original

--- frontend/software-extractor/src/components/CompareTwoResults.js
+++ frontend/software-extractor/src/components/CompareTwoResults.js
@@ -2,30 +2,29 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ClassComponent from './ClassComponent';
 // import SplitPane from "react-split-pane";
 
 class CompareTwoResults extends React.Component {
 
 
     render() {
+
+        console.log(this.props);
         return (
 
 
             <Container>
                 <h1>Compare Versions: </h1>
                 <Row>
-                    <Col style= {{border: '2px solid #000000'}}>{this.props.version1}</Col>
-                    <Col style={{border: '2px solid #000000'}}>{this.props.version2}</Col>
+                    <Col style= {{border: '2px solid #000000'}}>
+                        <ClassComponent key={this.props.version1.name} {...this.props.version1} />
+                    </Col>
+                    <Col style={{border: '2px solid #000000'}}>
+                    <ClassComponent key={this.props.version2.name} {...this.props.version2} />
+                    </Col>
                 </Row>
             </Container>
-            // <div>
-            //     {/* <SplitPane split='vertical' minSize={50} defaultSize={100}>
-            //         <div>Split 1</div>
-            //         <div>Split 2</div>
-            //     </SplitPane> */}
-            // </div>
-
-
         );
     }
 }
